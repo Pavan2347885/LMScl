3b01f5dc95457f7f9618eb0eaa5068a8e44fd249
Pavan <pavan10192003@gmail.com>
2025-05-14 14:59:02 +0530

    Initial commit7

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL ="https://lmsserver-epac.onrender.com";
 
 
 const api = axios.create({
